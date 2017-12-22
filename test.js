36dc31ad0ccd0dfb1a53fc3b5728d928d9a472ee
lrgeorge27 <lrgeorge27@gmail.com>
2017-12-22 04:32:42 +0000

    basic operations are functional

--- test.js
+++ test.js
@@ -1,14 +1,64 @@
 console.log("linked");
-var currentNum = "0";
+var currentNum = "";
 var memory = "";
 var dis = document.getElementById("dis");
+var total = "";
+var operator = "";
 
 function addNumber(id) {
     console.log("click");
     console.log(id);
     // var value = document.getElementById("9").value
-    currentNum = id;
+    currentNum += id;
     console.log(currentNum);
-    dis.value += currentNum;
+    dis.value = currentNum;
     // console.log(currentNum.toString());
+}
+
+function c() {
+    console.log("clear");
+    currentNum = "";
+    dis.value = "";
+    console.log(currentNum);
+}
+
+function clearAll() {
+    console.log("CE");
+    currentNum = "";
+    memory = "";
+    total = "";
+    dis.value = currentNum;
+    console.log(memory, total);
+}
+
+
+function operation(value) {
+    console.log("click op");
+    operator = value;
+    memory = currentNum + operator;
+    currentNum = "";
+    dis.value = memory;
+    console.log(memory, operator);
+}
+
+function equals() {
+    console.log("click =");
+    if (operator === "+") {
+        total = parseFloat(memory, 10) + parseFloat(currentNum, 10);
+        console.log(memory, currentNum);
+        console.log(total);
+    }
+    else if (operator === "-") {
+        total = parseFloat(memory, 10) - parseFloat(currentNum, 10);
+        console.log(total);
+    }
+    else if (operator === "*") {
+        total = parseFloat(memory, 10) * parseFloat(currentNum, 10);
+        console.log(total);
+    }
+    if (operator === "/") {
+        total = parseFloat(memory, 10) / parseFloat(currentNum, 10);
+        console.log(total);
+    }
+    dis.value = total;
 }
